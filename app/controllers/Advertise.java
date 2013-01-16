1bd5ba3259aa8b5ed256bc62de72cf7be8c67a2e
ranjith <gururanjith4@gmail.com>
2013-01-16 22:59:35 +0530

    Advertise

---
+++ app/controllers/Advertise.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import java.util.List;
+
+import models.AutoPost;
+import play.libs.Codec;
+import play.mvc.Controller;
+
+public class Advertise extends Controller{
+	public static void advertise() {
+		render();
+    }
+}
